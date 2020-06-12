b743e73bc0cedd244b14d2d616503b44eabebac4
Jorge Alexis Zanabria <63270774+ajmoose96@users.noreply.github.com>
2020-06-11 23:41:39 -0500

    prueba2

---
+++ precache-manifest.3c8a5bbeed6e6287ab9ff6f076b7ee59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b84c96d3303c7e2705fafd8bce38d05",
+    "url": "/Jorge-Alexis96/TareaReact2/index.html"
+  },
+  {
+    "revision": "1d21f741bbdaebabc22c",
+    "url": "/Jorge-Alexis96/TareaReact2/static/js/2.1eee867a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Jorge-Alexis96/TareaReact2/static/js/2.1eee867a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25511e877de36e318271",
+    "url": "/Jorge-Alexis96/TareaReact2/static/js/main.90b52345.chunk.js"
+  },
+  {
+    "revision": "fedbbfbc27854b05f860",
+    "url": "/Jorge-Alexis96/TareaReact2/static/js/runtime-main.0cdd3f57.js"
+  }
+]);
